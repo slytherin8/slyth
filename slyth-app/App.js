8e0e5e4de4898463892c4a52f593d38d54b62e10
bhanu-3333 <bhanuu339@gmail.com>
2026-01-17 21:08:26 +0530

    fix: resolve company logo and name fetch on web by using localhost API

--- slyth-app/App.js
+++ slyth-app/App.js
@@ -8,18 +8,45 @@ import AdminDashboard from "./src/screens/AdminDashboard";
 import CreateEmployeeScreen from "./src/screens/CreateEmployeeScreen";
 import EmployeeHome from "./src/screens/EmployeeHome";
 
+// Admin pages
+import AdminChatScreen from "./src/screens/AdminChatScreen";
+import AdminMeetScreen from "./src/screens/AdminMeetScreen";
+import AdminWorkScreen from "./src/screens/AdminWorkScreen";
+import AdminFilesScreen from "./src/screens/AdminFilesScreen";
+import AdminProfileScreen from "./src/screens/AdminProfileScreen";
+
+// Employee pages
+import EmployeeChatScreen from "./src/screens/EmployeeChatScreen";
+import EmployeeMeetScreen from "./src/screens/EmployeeMeetScreen";
+import EmployeeWorkScreen from "./src/screens/EmployeeWorkScreen";
+import EmployeeProfileScreen from "./src/screens/EmployeeProfileScreen";
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Auth */}
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="AdminSignup" component={AdminSignupScreen} />
+
+        {/* Admin */}
         <Stack.Screen name="AdminDashboard" component={AdminDashboard} />
         <Stack.Screen name="CreateEmployee" component={CreateEmployeeScreen} />
+        <Stack.Screen name="AdminChat" component={AdminChatScreen} />
+        <Stack.Screen name="AdminMeet" component={AdminMeetScreen} />
+        <Stack.Screen name="AdminWork" component={AdminWorkScreen} />
+        <Stack.Screen name="AdminFiles" component={AdminFilesScreen} />
+        <Stack.Screen name="AdminProfile" component={AdminProfileScreen} />
+
+        {/* Employee */}
         <Stack.Screen name="EmployeeHome" component={EmployeeHome} />
+        <Stack.Screen name="EmployeeChat" component={EmployeeChatScreen} />
+        <Stack.Screen name="EmployeeMeet" component={EmployeeMeetScreen} />
+        <Stack.Screen name="EmployeeWork" component={EmployeeWorkScreen} />
+        <Stack.Screen name="EmployeeProfile" component={EmployeeProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
