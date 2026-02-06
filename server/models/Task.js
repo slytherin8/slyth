db61be141efc694472a78e592c104eb747265f6f
bhanu-3333 <bhanuu339@gmail.com>
2026-02-06 21:55:05 +0530

    feat: implement initial application architecture including authentication, user roles, work management, and group functionalities.

---
+++ server/models/Task.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    employeeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["Pending", "Updating", "Completed"],
+        default: "Pending"
+    },
+    companyId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Company",
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Task", taskSchema);
