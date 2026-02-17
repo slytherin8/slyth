9d77c9a1dde1c3d92eade3a598610fe00ec42a06
bhanu-3333 <bhanuu339@gmail.com>
2026-02-17 22:21:43 +0530

    logic fixing

--- server/server.js
+++ server/server.js
@@ -46,7 +46,8 @@ const authLimiter = rateLimit({
 });
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // Routes
 app.use("/api/auth", authLimiter, authRoutes);
