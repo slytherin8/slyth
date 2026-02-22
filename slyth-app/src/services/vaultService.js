2bf5edf9b7d050e21cf8b0667450dd2c493098c8
bhanu-3333 <bhanuu339@gmail.com>
2026-02-22 13:23:22 +0530

    feat: Add Admin Meet screen for Google Meet integration and a vault service for secure file and folder management with PIN capabilities.

--- slyth-app/src/services/vaultService.js
+++ slyth-app/src/services/vaultService.js
@@ -1,5 +1,5 @@
 import api from "./api";
-import * as FileSystem from "expo-file-system";
+import * as FileSystem from "expo-file-system/legacy";
 import * as Sharing from "expo-sharing";
 import { Platform } from "react-native";
 
